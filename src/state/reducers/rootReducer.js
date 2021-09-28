8b24074bef27dfd06db1653109186aeffe0a02b8
shadman <shadmanshahriar2011@gmail.com>
2021-09-28 10:31:12 +0600

    modal reducer removed

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import productReducer from "./productReducer";
 import cartReducer from "./cartReducer";
-import modalReducer from "./modalReducer";
 
 const rootReducer = combineReducers({
     productReducer,
     cartReducer,
-    modalReducer
 });
 
 export default rootReducer
